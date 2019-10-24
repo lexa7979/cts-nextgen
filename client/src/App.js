73fccd303453b00743edf74dd1afae0b87c42699
Alexander Urban <lexa@toxa.de>
2019-10-24 16:00:41 +0200

    Client: Checking server state

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,34 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { useState, useEffect } from "react";
+import "./App.scss";
 
 /**
  * Main React.js component
  */
 function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
+	const [ serverPingInterval, setServerPingInterval ] = useState( null );
+	const [ serverState, setServerState ] = useState( false );
+
+	useEffect( () => {
+		if ( serverPingInterval == null ) {
+			setServerPingInterval( setInterval( async () => {
+				const response = await fetch( "/health" );
+				if ( response.status === 200 ) {
+					const output = await response.text();
+					setServerState( output === "OK" );
+				} else {
+					setServerState( false );
+				}
+			}, 3000 ) );
+		}
+
+		return () => clearInterval( serverPingInterval );
+	}, [ serverPingInterval ] );
+
+	return <div className="App">
+		<div className={`server-state ${serverState ? "up" : "down"}`}>
+			{serverState ? "Server is UP" : "Server is DOWN"}
 		</div>
-	);
+	</div>;
 }
 
 export default App;
